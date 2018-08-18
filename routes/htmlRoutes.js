53836645048fb9d1acc2ed07575070ecddc5ebec
Alex Federovitch <alexfederovitch@yahoo.com>
2018-08-17 23:46:09 -0500

    making adjustments to the apiRoutes and htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function(_req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
@@ -21,7 +21,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function(_req, res) {
     res.render("404");
   });
 };
