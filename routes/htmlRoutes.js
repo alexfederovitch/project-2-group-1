01b3e969bfa3ff65dfff9b0c2755ba243259fb18
Andrew Duong <andrewduong94@gmail.com>
2018-08-22 22:22:36 -0500

    updated

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,40 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(_req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+  //load Home page
+  app.get("/", function(req, res, next) {
+    res.render("index");
+  });
+  //load Signup page
+  app.get("/signup", function(req, res, next) {
+    res.render("create-account");
+  });
+  //load Login page
+  app.get("/login", function(req, res, next) {
+    res.render("login");
+  });
+  // Load meal planner
+  app.get("/user/meal", function(req, res) {
+    db.meals
+      .findAll({ order: [["mealOrder", "ASC"]] })
+      .then(function(userMeals) {
+        res.render("meal-planner", {
+          msg: "Welcome!",
+          meals: userMeals
+        });
       });
-    });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
+  //LOAD meal planner for specific day of the week
+  app.get("user/meal/:day", function(req, res) {
+    db.meals
+      .findAll({ where: { day: req.params.day } })
+      .then(function(userMeals) {
+        res.render("meal-planner", {
+          msg: "Welcome!",
+          meals: userMeals
+        });
       });
-    });
   });
 
   // Render 404 page for any unmatched routes
