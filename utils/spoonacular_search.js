7f85a0a0800941e93d7c5aa3d7c54b0b6b60c1e1
petra21a <35877270+petra21a@users.noreply.github.com>
2018-08-24 22:18:28 -0500

    small edits and moved mealplan totals to top of page

--- utils/spoonacular_search.js
+++ utils/spoonacular_search.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const request = require('request');
 
 module.exports = function (query, callback) {
@@ -71,68 +70,3 @@ module.exports = function (query, callback) {
     }
     getSearch();
 }
-=======
-const request = require("request");
-
-module.exports = function(query, callback) {
-  let mealsFound = [];
-  let searchList = [];
-  getSearch = function() {
-    const search = {
-      url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/recipes/search?
-            number=10&query=${query}&limitLicense=false`,
-      headers: {
-        "X-Mashape-Key": process.env.MASH_APIKEY,
-        "X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-      }
-    };
-
-    request.get(search, function(error, response, body) {
-      let searchList = JSON.parse(body);
-      // console.log(snackList.results[0].id,snackList.results.length)
-
-      for (let i = 0; i < searchList.results.length; i++) {
-        mealsFound.push(
-          {
-          spoontacularID: searchList.results[i].id
-        });
-      }
-      // console.log(mealsSnack);
-      getDetails();
-    });
-  };
-  const details = {
-    url:
-      "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk?" +
-      // `ids=576710%2C10341&includeNutrition=true`,
-      `ids=${mealsFound.join("%2C")}&includeNutrition=true`,
-    headers: {
-      "X-Mashape-Key": process.env.MASH_APIKEY,
-      "X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-    }
-  };
-  // console.log(details);
-
-
-
-
-  request.get(details, function (error, response, body) {
-    let detailsList = JSON.parse(body);
-    for (let i = 0; i < detailsList.length; i++) {
-      // console.log(mealsDetails[i])
-      mealsDetails[i] = Object.assign(searchList[i], {
-        mealName: detailsList[i].title,
-        image: detailsList[i].image,
-        url: detailsList[i].spoonacularSourceUrl,
-        calories: detailsList[i].nutrition.nutrients[0].amount,
-        fat: detailsList[i].nutrition.nutrients[1].amount,
-        carbs: detailsList[i].nutrition.nutrients[3].amount,
-        protein: detailsList[i].nutrition.nutrients[7].amount
-      });
-    }
-    callback(mealsDetails);
-  });
-  getSearch();
-};
-
->>>>>>> 8f0333a4ecdb40c385a1ec3bf01a053196797283
