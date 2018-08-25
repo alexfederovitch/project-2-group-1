f6e6887421d37cfa96156c1f309eef30405351c5
Andrew Duong <andrewduong94@gmail.com>
2018-08-25 02:52:38 -0500

    updated

--- public/js/createAccount.js
+++ public/js/createAccount.js
@@ -1,35 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var config = {
-  apiKey: "AIzaSyBLBrmK0uxoh_3-cXZrtZWVmShKBnPunFw",
-  authDomain: "macrosapp-ea2a8.firebaseapp.com",
-  databaseURL: "https://macrosapp-ea2a8.firebaseio.com",
-  projectId: "macrosapp-ea2a8",
-  storageBucket: "macrosapp-ea2a8.appspot.com",
-  messagingSenderId: "498399742442"
-};
-firebase.initializeApp(config);
+    apiKey: "AIzaSyBLBrmK0uxoh_3-cXZrtZWVmShKBnPunFw",
+    authDomain: "macrosapp-ea2a8.firebaseapp.com",
+    databaseURL: "https://macrosapp-ea2a8.firebaseio.com",
+    projectId: "macrosapp-ea2a8",
+    storageBucket: "macrosapp-ea2a8.appspot.com",
+    messagingSenderId: "498399742442"
+  };
+  firebase.initializeApp(config);
 
-const auth = firebase.auth();
+  const auth = firebase.auth();
 });
 
-$("#logOut").on("click", function (event) {
+$("#logOut").on("click", function(event) {
   event.preventDefault();
-  console.log("hello from logout")
-  firebase.auth().signOut().then(function(){localStorage.clear();window.location.href = "/";}).catch(function (error) {
+  console.log("hello from logout");
+  firebase
+    .auth()
+    .signOut()
+    .then(function() {
+      localStorage.clear();
+      window.location.href = "/";
+    })
+    .catch(function(error) {
       console.log(error.message);
-  });
-   });
+    });
+});
 //Onclick for the Submit Button for th Sign Up
 $("#button").on("click", function(event) {
   event.preventDefault();
   //Grab the email and password values
-  let firstName = $("#first-name").val().trim();
-  let lastName = $("#last-name").val().trim();
-  let email = $("#email").val().trim();
-  let username = $("#username").val().trim();
-  let pw1 = $("#password").val().trim();
-  let pw2 = $("#confirm-password").val().trim();
-  let currentWeight = $("#weight").val().trim();
+  let firstName = $("#first-name")
+    .val()
+    .trim();
+  let lastName = $("#last-name")
+    .val()
+    .trim();
+  let email = $("#email")
+    .val()
+    .trim();
+  let username = $("#username")
+    .val()
+    .trim();
+  let pw1 = $("#password")
+    .val()
+    .trim();
+  let pw2 = $("#confirm-password")
+    .val()
+    .trim();
+  let currentWeight = $("#weight")
+    .val()
+    .trim();
   let goal = $("input[name='goalOption']:checked").val();
 
   console.log(email, username, pw1, pw2, currentWeight, goal);
@@ -48,9 +69,11 @@ $("#button").on("click", function(event) {
   /////////////\\\\\\\\\\\\\\\
   //VALIDATE that pw1 = pw2 BEFORE sending the POST req!!!!!!
   ///////////////\\\\\\\\\\\\\\\
-var temp;
+  var temp;
   if (newUserVals.password === newUserVals.password2) {
-    firebase.auth().createUserWithEmailAndPassword(newUserVals.email, newUserVals.password)
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUserVals.email, newUserVals.password)
       .then(function(user) {
         // This POST request is sent to the API Routes.JS
         $.ajax("/api/newUser", {
@@ -67,13 +90,13 @@ var temp;
             localStorage.setItem("username", result.dbUsers.username);
             localStorage.setItem("firstName", result.dbUsers.firstName);
             localStorage.setItem("currenWeight", result.dbUsers.currentWeight);
-            window.location.href= `/members/${localStorage.id}`
+            window.location.href = `/members/${localStorage.id}`;
           }
         });
       })
       .catch(function(result) {
         console.log(result.msg);
-        alert(result.msg.message);
+        alert(result.message);
         //this function is caputring the error and
         console.log("line 151 on index.handlebars");
         console.log(result);
