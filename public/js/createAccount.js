74c4d0cca4c3240c85d48f6072ff087a39d8a248
petra21a <35877270+petra21a@users.noreply.github.com>
2018-08-25 02:25:37 -0500

    deleted .msg from line 80 of createAccounts.js

--- public/js/createAccount.js
+++ public/js/createAccount.js
@@ -1,26 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
   var config = {
-  apiKey: "AIzaSyBLBrmK0uxoh_3-cXZrtZWVmShKBnPunFw",
-  authDomain: "macrosapp-ea2a8.firebaseapp.com",
-  databaseURL: "https://macrosapp-ea2a8.firebaseio.com",
-  projectId: "macrosapp-ea2a8",
-  storageBucket: "macrosapp-ea2a8.appspot.com",
-  messagingSenderId: "498399742442"
-};
-firebase.initializeApp(config);
+    apiKey: "AIzaSyBLBrmK0uxoh_3-cXZrtZWVmShKBnPunFw",
+    authDomain: "macrosapp-ea2a8.firebaseapp.com",
+    databaseURL: "https://macrosapp-ea2a8.firebaseio.com",
+    projectId: "macrosapp-ea2a8",
+    storageBucket: "macrosapp-ea2a8.appspot.com",
+    messagingSenderId: "498399742442"
+  };
+  firebase.initializeApp(config);
 
-const auth = firebase.auth();
+  const auth = firebase.auth();
 });
 
 $("#logOut").on("click", function (event) {
   event.preventDefault();
   console.log("hello from logout")
-  firebase.auth().signOut().then(function(){localStorage.clear();window.location.href = "/";}).catch(function (error) {
-      console.log(error.message);
+  firebase.auth().signOut().then(function () { localStorage.clear(); window.location.href = "/"; }).catch(function (error) {
+    console.log(error.message);
   });
-   });
-//Onclick for the Submit Button for th Sign Up
-$("#button").on("click", function(event) {
+});
+
+//Onclick for the Submit Button for the Sign Up
+$("#button").on("click", function (event) {
   event.preventDefault();
   //Grab the email and password values
   let firstName = $("#first-name").val().trim();
@@ -32,7 +33,7 @@ $("#button").on("click", function(event) {
   let currentWeight = $("#weight").val().trim();
   let goal = $("input[name='goalOption']:checked").val();
 
-  console.log(email, username, pw1, pw2, currentWeight, goal);
+  console.log(firstName,lastName, email, username, pw1, pw2, currentWeight, goal);
 
   const newUserVals = {
     firstName: firstName,
@@ -48,15 +49,18 @@ $("#button").on("click", function(event) {
   /////////////\\\\\\\\\\\\\\\
   //VALIDATE that pw1 = pw2 BEFORE sending the POST req!!!!!!
   ///////////////\\\\\\\\\\\\\\\
-var temp;
+  
+  //__>DELETE?
+  var temp;
+console.log(newUserVals.password,newUserVals.password2,newUserVals.password === newUserVals.password2)
   if (newUserVals.password === newUserVals.password2) {
     firebase.auth().createUserWithEmailAndPassword(newUserVals.email, newUserVals.password)
-      .then(function(user) {
+      .then(function (user) {
         // This POST request is sent to the API Routes.JS
         $.ajax("/api/newUser", {
           type: "POST",
           data: newUserVals
-        }).then(function(result) {
+        }).then(function (result) {
           if (result.isworking) {
             console.log(result);
             temp = result;
@@ -67,13 +71,13 @@ var temp;
             localStorage.setItem("username", result.dbUsers.username);
             localStorage.setItem("firstName", result.dbUsers.firstName);
             localStorage.setItem("currenWeight", result.dbUsers.currentWeight);
-            window.location.href= `/members/${localStorage.id}`
+            window.location.href = `/members/${localStorage.id}`
           }
         });
       })
-      .catch(function(result) {
-        console.log(result.msg);
-        alert(result.msg.message);
+      .catch(function (result) {
+        console.log(result);
+        alert(result.message);
         //this function is caputring the error and
         console.log("line 151 on index.handlebars");
         console.log(result);
